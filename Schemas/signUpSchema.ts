b8a7f32a4a9ef30456a8eee890d377113ab7f2e0
Arjun gupta <rachnag352@gmail.com>
2026-02-18 22:44:08 +0530

    added schema validation using zod

--- Schemas/signUpSchema.ts
+++ Schemas/signUpSchema.ts
@@ -1,8 +1,20 @@
 import * as z from "zod";
-import { email } from "zod/v4";
 
-export const signUpSchema = z.object({
-  password: z
-    .string()
-    .length(4, { message: "password should be atleast of 4 characters" }),
-});
+export const signUpSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: "Email is required" })
+      .email({ message: "Please enter a valid email address" }),
+    password: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .min(8, { message: "Password must be at least 8 characters" }),
+    passwordConfirmation: z
+      .string()
+      .min(1, { message: "Please confirm your password" }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+    path: ["passwordConfirmation"],
+  });
