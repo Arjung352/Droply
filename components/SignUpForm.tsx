69116731bc5c6f6f457911c1cf675606b1514f0f
Arjun gupta <rachnag352@gmail.com>
2026-03-27 07:16:54 +0530

    implemented the logic of react-hook-form

--- components/SignUpForm.tsx
+++ components/SignUpForm.tsx
@@ -5,9 +5,32 @@ import { z } from "zod";
 
 // importing zod schema
 import { signUpSchema } from "@/Schemas/signUpSchema";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function SignUpForm() {
+  const [verifying, setVerifying] = useState(false);
   const { signUp, isLoaded, setActive } = useSignUp();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    },
+  });
+
   const onSubmit = async () => {};
   const handleVerificationSubmit = async () => {};
+
+  if (verifying) {
+    return <h1>This is otp entering field</h1>;
+  }
+
+  return <h1>SignUp form with email and other fields</h1>;
 }
